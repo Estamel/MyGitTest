7923e5dd471b55b2ffa9a7d0277d83ca298632cf
Your Name <you@example.com>
2021-03-04 18:19:28 +0800

    Java学习第22天

--- day020/src/test08/Main.java
+++ day020/src/test08/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(Season.SPRING.toString());
         System.out.println(Season.SUMMER.toString());
+        System.out.println(Season.SUMMER.toString());
     }
 }
