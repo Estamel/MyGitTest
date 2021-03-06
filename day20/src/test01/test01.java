c5f17dce6a02d318f1178cb8b27814f5b6dc435e
Your Name <you@example.com>
2021-03-06 18:51:26 +0800

    Java学习第5天

--- day20/src/test01/test01.java
+++ day20/src/test01/test01.java
@@ -4,5 +4,7 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
     }
 }
